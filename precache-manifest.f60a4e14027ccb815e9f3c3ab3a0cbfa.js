fe8b2138871368c4a2b34cf344f4ce5c6935d5f2
Gustavo <gustavo.velazquezm@outlook.com>
2020-08-02 23:44:53 -0500

    Updates

---
+++ precache-manifest.f60a4e14027ccb815e9f3c3ab3a0cbfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ff44e5fc91da68ca576b153d50e162",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7dceaafedc24f1ec18f",
+    "url": "./static/css/main.fca31358.chunk.css"
+  },
+  {
+    "revision": "e108ca4f2951226879ac",
+    "url": "./static/js/2.a10dab2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a10dab2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7dceaafedc24f1ec18f",
+    "url": "./static/js/main.6eaf482f.chunk.js"
+  },
+  {
+    "revision": "6f73855c737ab9130b54",
+    "url": "./static/js/runtime-main.fa399fd1.js"
+  }
+]);
